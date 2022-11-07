677c44b5c6692e5bef408f2c2f788b54272232e5
SOURCESEM0 <116640043+SOURCESEM0@users.noreply.github.com>
2022-11-07 15:35:30 +0200

    Update __init__.py

--- zthon/__init__.py
+++ zthon/__init__.py
@@ -14,7 +14,7 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.1.1"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "ZThon <https://github.com/Zed-Thon/Zelzal>"
+__author__ = "ZThon <https://github.com/SOURCESEM0/Z_S>"
 __copyright__ = f"ZThon Copyright (C) 2021 - 2022  {__author__}"
 
 zedub.version = __version__
@@ -35,7 +35,7 @@ def close_connection(*_):
 signal.signal(signal.SIGTERM, close_connection)
 
 if Config.UPSTREAM_REPO == "zel":
-    UPSTREAM_REPO_URL = "https://github.com/Zed-Thon/ZelZal"
+    UPSTREAM_REPO_URL = "https://github.com/SOURCESEM0/Z_S"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
