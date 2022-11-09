5a4efc1b8c2d38e45c6786d45cacd71b1a899900
SOURCESEM0 <116640043+SOURCESEM0@users.noreply.github.com>
2022-11-09 16:33:30 +0200

    Update المعلومات.py

--- zthon/plugins/المعلومات.py
+++ zthon/plugins/المعلومات.py
@@ -77,7 +77,7 @@ async def _(event):
 async def _(event):
     cmd = "ls zthon/plugins"
     o = (await _zedutils.runcmd(cmd))[0]
-    OUTPUT = f"**⌔∮ [𝗦𝗢𝗨𝗥𝗖𝗘 𝙕𝞝𝘿](tg://need_update_for_some_feature/) الالاضافات:**\n{o}"
+    OUTPUT = f"**⌔∮ [ 𝐒𝐎𝐔𝐑𝐂𝐄 𝐒𝐄𝐌𝐎](tg://need_update_for_some_feature/) الالاضافات:**\n{o}"
     await edit_or_reply(event, OUTPUT)
 
 
@@ -129,7 +129,7 @@ async def _(event):
     stdout, stderr = await process.communicate()
     o = stdout.decode()
     OUTPUT = (
-        f"**[𝗦𝗢𝗨𝗥𝗖𝗘 𝙕𝞝𝘿](tg://need_update_for_some_feature/) Environment Module:**\n\n\n{o}"
+        f"**[𝐒𝐎𝐔𝐑𝐂𝐄 𝐒𝐄𝐌𝐎 ](tg://need_update_for_some_feature/) Environment Module:**\n\n\n{o}"
     )
     if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
@@ -186,7 +186,7 @@ async def _(event):
 @zedub.zed_cmd(pattern="تاريخ التنصيب$")
 async def zeddd(event):
     uname = platform.uname()
-    zedt = "**- تاريخ تنصيبـك لـ بـوت زدثـــون - 𓆩𝙎𝙊𝙐𝙍𝘾𝞝 𝙕𝞝𝘿𓆪**\n\n"
+    zedt = "**- تاريخ تنصيبـك لـ بـوت سيمو - • 𝐒𝐎𝐔𝐑𝐂𝐄 𝐒𝐄𝐌𝐎 • **\n\n"
     boot_time_timestamp = psutil.boot_time()
     zz = datetime.fromtimestamp(boot_time_timestamp)
     zedt += f"**هـو** ` {zz.year}/{zz.month}/{zz.day} `"
