e485facd2fb2ac51d96029af6a5cca36f8967bc8
SOURCESEM0 <116640043+SOURCESEM0@users.noreply.github.com>
2022-11-12 11:42:09 +0200

    Update المليار.py

--- zthon/plugins/المليار.py
+++ zthon/plugins/المليار.py
@@ -1,6 +1,6 @@
 #by Hussein For Jepthon-Joker
 # يمنع منعاً باتاً تخمط الملف خلي عندك كرامه ولتسرقة
-from jepthon import jepiq
+from jepthon import zedub
 from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
@@ -12,17 +12,17 @@ bot_username = '@t06bot'
 jepthon = ['yes']
 
 
-@jepiq.on(admin_cmd(pattern="(تجميع النقاط|تجميع نقاط)"))
+@zedub.on(admin_cmd(pattern="(تجميع النقاط|تجميع نقاط)"))
 async def _(event):
     if jepthon[0] == "yes":
         await event.edit("**᯽︙سيتم تجميع النقاط , قبل كل شي تأكد من انك قمت بلانظمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
-        channel_entity = await jepiq.get_entity(bot_username)
-        await jepiq.send_message('@t06bot', '/start')
+        channel_entity = await zedub.get_entity(bot_username)
+        await zedub.send_message('@t06bot', '/start')
         await asyncio.sleep(5)
         msg0 = await jepiq.get_messages('@t06bot', limit=1)
         await msg0[0].click(2)
         await asyncio.sleep(5)
-        msg1 = await jepiq.get_messages('@t06bot', limit=1)
+        msg1 = await zedub.get_messages('@t06bot', limit=1)
         await msg1[0].click(0)
 
         chs = 1
@@ -31,25 +31,25 @@ async def _(event):
                 break
             await asyncio.sleep(5)
 
-            list = await jepiq(GetHistoryRequest(peer=channel_entity, limit=1,
+            list = await zedub(GetHistoryRequest(peer=channel_entity, limit=1,
                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
             if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
-                await jepiq.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
+                await zedub.send_message(event.chat_id, f"**لاتوجد قنوات للبوت**")
                 break
             url = msgs.reply_markup.rows[0].buttons[0].url
             try:
                 try:
-                    await jepiq(JoinChannelRequest(url))
+                    await zedub(JoinChannelRequest(url))
                 except:
                     bott = url.split('/')[-1]
                     await jepiq(ImportChatInviteRequest(bott))
                 msg2 = await jepiq.get_messages('@t06bot', limit=1)
                 await msg2[0].click(text='تحقق')
                 chs += 1
-                await jepiq.send_message("me", f"تم الاشتراك في {chs} قناة")
+                await zedub.send_message("me", f"تم الاشتراك في {chs} قناة")
             except:
-                await jepiq.send_message(event.chat_id, f"**خطأ , ممكن تبندت**")
+                await zedub.send_message(event.chat_id, f"**خطأ , ممكن تبندت**")
                 break
         await jepiq.send_message(event.chat_id, "**تم الانتهاء من التجميع !**")
 
